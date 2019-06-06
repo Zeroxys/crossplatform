6a37bf2b6c53d1f09ae5d2add2a58792a6bce209
Miguel <miguelzavalac@gmail.com>
2019-06-05 20:52:21 -0500

    first screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import background from '../public/images/LifeIsGigaaa.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainContent">
+      
+      <p>I have something to tell you</p>
+      <img src={background} className="logoGigaaa"></img> 
+      <p>Tap anywhere on the screen if you're not available to listen</p>
+
     </div>
   );
 }
