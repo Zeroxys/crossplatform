18b3e5be56ff42a81636645a74b5f3229a75a82d
Miguel <miguel.z@adestech.mx>
2019-06-10 16:58:12 -0500

    check the transition group

--- src/Components/Tips/Tips.js
+++ src/Components/Tips/Tips.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, {useState, Fragment} from 'react';
 import Musica from '../../../public/images/musica.png';
+import {CSSTransition} from 'react-transition-group'
 import './Tips.css'
 
 export default function Tips () {
+
+    const [inProp, setInProp] = useState(false)
+
     return (
-        <div className="TipsMainContent">
-            <div className="infoContainer">
-                <h2>Get it Done Right Away</h2>
-                <p className="subtitle">Helpful services to go through your daily tasks</p>
+        
+<Fragment>
 
-                <img src={Musica} className="Musica"></img>
+        <CSSTransition>
+            <div className="TipsMainContent">
+                <div className="infoContainer">
+                    <h2>Get it Done Right Away</h2>
+                    <p className="subtitle">Helpful services to go through your daily tasks</p>
 
-                <p className="infoTips">"Do I need an umbrella?"</p>
-                <p className="infoTips">"Find a parking spot nearby."</p>
-                <p className="infoTips"> "Play my Reading playlist on Sportify."</p>
+                    <img src={Musica} className="Musica"></img>
+
+                    <p className="infoTips">"Do I need an umbrella?"</p>
+                    <p className="infoTips">"Find a parking spot nearby."</p>
+                    <p className="infoTips"> "Play my Reading playlist on Sportify."</p>
+                </div>
             </div>
-        </div>
+        </CSSTransition>
+</Fragment>
     )
 }
