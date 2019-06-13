3d18d3b3f562def4b6f8010af57302532c103f30
Miguel <miguel.z@adestech.mx>
2019-06-13 10:52:16 -0500

    given error message on loginForm

--- src/Components/Login/LogInUser.js
+++ src/Components/Login/LogInUser.js
@@ -33,8 +33,9 @@ export default function LogInUser(props) {
                 <div className="checkContent">
                 <input id="checkRemember" type="checkbox" name="Remember" value="Remember"/>
                 <span className="checkmark"></span>
-                <label >{props.errorMessage}</label>                
                 <label htmlFor="checkRemember">Remember</label>
+
+                <label style ={{color :'red', display:'block'}}>{props.errorMessage}</label>
                 </div>
 
                 {isLogin}
