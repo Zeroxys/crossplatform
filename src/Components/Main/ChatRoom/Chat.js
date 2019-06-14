aa4b835472dcdea71e53f2782e3dc83e8feca9de
Miguel <miguel.z@adestech.mx>
2019-06-14 16:51:37 -0500

    check the last commit

--- src/Components/Main/ChatRoom/Chat.js
+++ src/Components/Main/ChatRoom/Chat.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import iconGigaaa from '../../../../public/images/gigaaalogoIcon.png'
 
-export default function Chat () {
+export default function Chat (props) {
+
+
+    let isUser = props.isUser ? '' : <img style={{width:'15%'}} src={iconGigaaa}></img>
+
     return (
         <div className="speech">
-            <img style={{width:'15%'}} src={iconGigaaa}></img>
+            {isUser}
             <div className="speech-bubble">
                 <p>It is equal to 627.</p>
             </div>
