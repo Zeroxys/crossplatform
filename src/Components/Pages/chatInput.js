fce5c1de434438249bdb680aa61a027bbd485e6f
Miguel <miguel.z@adestech.mx>
2019-06-18 16:08:21 -0500

    chat input fixed

--- src/Components/Pages/chatInput.js
+++ src/Components/Pages/chatInput.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import '../../App.css';
-
-
-const styles = {
-    backgroundColor : 'transparent'
-}
 
 export default function chatInput (props) {
     return (
         <form className="chatInput">
             <div className="formTypeHere">
 
-                <input placeholder="Type here..." defaultValue={props.text}/>
-                <input style={styles} type="submit" value=">" onClick={ e => props.sendAudio(e) } ></input>
+                <input className="typeHere" placeholder="Type here..." defaultValue={props.text}/>
+                <input className="typeHereSubmit" type="submit" value=">" onClick={ e => props.sendAudio(e) } ></input>
 
             </div>
         </form>
