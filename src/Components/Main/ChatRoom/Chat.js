7f8d29e9e1b453f2f3f592d9ca0d0bb0bbdcfa50
Hector Lobato Silva <lobato_man@hotmail.com>
2019-06-18 16:19:34 -0500

    imagenes en mensaje

--- src/Components/Main/ChatRoom/Chat.js
+++ src/Components/Main/ChatRoom/Chat.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import iconGigaaa from '../../../../public/images/gigaaalogoIcon.png'
+import './chat.css'
 
 export default function Chat (props) {
 
 
     let isUser = props.isUser ? '' : <img style={{width:'15%'}} src={iconGigaaa}></img>
 
+    const { json } = props
+
     return (
         <div className="speech">
             {isUser}
             <div className="speech-bubble">
-                <p>is equal to 6</p>
+                {
+                    // For books
+                    json.map( item => <p>
+                        <img key = { item.id } src = { item.image } className = "speech_image" />
+                        <p className = "text_left">
+                            <p> <strong> { item.type_title } - { item.id } </strong> </p>
+                            <p> { item.author } - { item.title } - { item.text } </p>
+                        </p>
+                        <br/>
+                        <p>&nbsp;</p>
+                    </p>)
+                }
+                {/* {
+                    // For sports
+                    json.map( item => <p className = "speech_image_sport_container" >
+                        <img key = { item.id } src = { item.image } className = "speech_image_sport" />
+                        <div className = "speech_image_sport_text">
+                            <h3> { item.title_sport } </h3>
+                            <div> { item.date } </div>
+                            <div> { item.place } </div>
+                            <p>&nbsp;</p>
+                        </div>
+                    </p> )
+                } */}
             </div>
         </div>
     )
